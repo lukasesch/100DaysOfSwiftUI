5d6fd30bc06636806722444b8bd315f340129657
lukasesch <lukasesch@gmail.com>
2024-07-26 22:16:28 +0200

    Added Challange Files - Fun game to practice multiplication tables

---
+++ MultiplicationFun/MultiplicationFun/MultiplicationFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationFunApp.swift
+//  MultiplicationFun
+//
+//  Created by Lukas Esch on 26.07.24.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
